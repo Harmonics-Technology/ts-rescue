ae0cc0c092ffae6af27350334b40ce258c44cad1
interns <interns@bts.com.ng>
2023-03-02 15:02:22 +0100

    added recent payrolls and fixed order by recent timesheet on admin dashboard

--- Models/ViewModels/DashboardPaymentPartnerView.cs
+++ Models/ViewModels/DashboardPaymentPartnerView.cs
@@ -4,7 +4,7 @@ namespace TimesheetBE.Models.ViewModels
 {
     public class DashboardPaymentPartnerView
     {
-        public List<RecentPayrollView> RecentPayroll { get; set; }
+        public List<InvoiceView> RecentPayroll { get; set; }
         public List<InvoiceView> RecentApprovedInvoice { get; set; }
         public List<InvoiceView> RecentInvoicedInvoice { get; set; }
         //public List<RecentInvoiceView> RecentInvoice { get; set; }
