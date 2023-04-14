0169387459284f6af5cc7ad3f88ac31ebeb0244d
interns <interns@bts.com.ng>
2023-04-14 18:26:04 +0100

    work-assignee email

--- Models/ViewModels/NotificationView.cs
+++ Models/ViewModels/NotificationView.cs
@@ -14,6 +14,6 @@ namespace TimesheetBE.Models.ViewModels
         public Guid UserId { get; set; }
         public User User { get; set;}
         public bool IsRead { get; set; }
-        public DateTime DateCreated { get; set; };
+        public DateTime DateCreated { get; set; }
     }
 }
