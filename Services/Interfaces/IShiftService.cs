141867a30e8b320c756114a51b2dd5fa7e1a85fc
interns <interns@bts.com.ng>
2023-04-17 22:43:27 +0100

    filter user by shift

--- Services/Interfaces/IShiftService.cs
+++ Services/Interfaces/IShiftService.cs
@@ -15,7 +15,7 @@ namespace TimesheetBE.Services.Interfaces
         Task<StandardResponse<List<ShiftView>>> ListUsersShift(UsersShiftModel model, bool? isPublished = null);
         //Task<StandardResponse<PagedCollection<UsersShiftView>>> ListUsersShift(PagingOptions pagingOptions, UsersShiftModel model, Guid? filterUserId = null);
         //Task<StandardResponse<List<ShiftView>>> ListUsersShift(UsersShiftModel model);
-        Task<StandardResponse<PagedCollection<UsersShiftView>>> GetUsersShift(PagingOptions pagingOptions, UsersShiftModel model, Guid? filterUserId = null);
+        Task<StandardResponse<PagedCollection<UsersShiftView>>> GetUsersShift(PagingOptions pagingOptions, UsersShiftModel model);
         Task<StandardResponse<PagedCollection<ShiftView>>> GetUserShift(PagingOptions pagingOptions, UsersShiftModel model);
         Task<StandardResponse<bool>> DeleteShift(Guid id);
         Task<StandardResponse<bool>> PublishShifts(DateTime startDate, DateTime endDate);
