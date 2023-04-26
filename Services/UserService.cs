3fa1e04d4687b1cb8be497ea78253086e5183b88
interns <interns@bts.com.ng>
2023-04-26 16:56:41 +0100

    commit

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -419,6 +419,7 @@ namespace TimesheetBE.Services
             mapped.NumberOfHoursEligible = employeeInformation?.NumberOfHoursEligible;
             mapped.EmployeeType = employeeInformation.EmployeeType;
 
+            //check if employeeinformation is null
             if (employeeInformation != null)
             {
                 var getNumberOfDaysEligible = _leaveService.GetEligibleLeaveDays(employeeInformation.Id);
