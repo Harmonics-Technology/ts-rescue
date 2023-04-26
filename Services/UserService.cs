819b5f42cba84c07f315cb644bb7aed12745ece2
interns <interns@bts.com.ng>
2023-04-26 16:34:29 +0100

    fixed super admin login bug

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -419,12 +419,11 @@ namespace TimesheetBE.Services
             mapped.NumberOfHoursEligible = employeeInformation?.NumberOfHoursEligible;
             mapped.EmployeeType = employeeInformation.EmployeeType;
 
-            var getNumberOfDaysEligible = _leaveService.GetEligibleLeaveDays(employeeInformation.Id);
-
-            mapped.NumberOfDaysEligible = getNumberOfDaysEligible - employeeInformation.NumberOfEligibleLeaveDaysTaken;
-
             if (employeeInformation != null)
             {
+                var getNumberOfDaysEligible = _leaveService.GetEligibleLeaveDays(employeeInformation.Id);
+
+                mapped.NumberOfDaysEligible = getNumberOfDaysEligible - employeeInformation?.NumberOfEligibleLeaveDaysTaken;
                 mapped.ClientId = employeeInformation?.ClientId;
             }
 
