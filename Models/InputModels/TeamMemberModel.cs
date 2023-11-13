394d6dc64da23d5f9b1b2ec6c00989627cd48e79
interns <interns@bts.com.ng>
2023-11-13 19:18:52 +0100

    update teamember model

--- Models/InputModels/TeamMemberModel.cs
+++ Models/InputModels/TeamMemberModel.cs
@@ -29,7 +29,7 @@ namespace TimesheetBE.Models.InputModels
         public double? MonthlyPayoutRate { get; set; }
         public string PaymentFrequency { get; set; }
         public bool IsActive { get; set; }
-        public double onBordingFee { get; set; }
+        public double OnBoradingFee { get; set; }
         //public DateTime? TimeSheetGenerationStartDate { get; set; }
         public bool? IsEligibleForLeave { get; set; }
         public int? NumberOfDaysEligible { get; set; }
