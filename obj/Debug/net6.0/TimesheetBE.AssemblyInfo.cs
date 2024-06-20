5ae5147cb41cae72f7f5c75467617f69da19512d
Muhammad Aderoju <aderojumuhammad6@gmail.com>
2024-06-20 16:52:06 +0100

    commit build

--- obj/Debug/net6.0/TimesheetBE.AssemblyInfo.cs
+++ obj/Debug/net6.0/TimesheetBE.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TimesheetBE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc93713930ef023848f6e2eb0175240d2bb3819b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91fcdb6de7a349ba3b2dc2e26b43ba33ff3b2473")]
 [assembly: System.Reflection.AssemblyProductAttribute("TimesheetBE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TimesheetBE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
